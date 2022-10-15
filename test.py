b3b3d6e4485d96f598f0cc16e78ac6246ef9aec6
anjie <anjiehst@gmail.com>
2022-10-15 16:27:34 -0500

    menus

--- test.py
+++ test.py
@@ -9,16 +9,29 @@ from tempfile import mkdtemp
 
 url = "https://raw.githubusercontent.com/anjieliuanjie/HackTX/main/Data%20for%20repository.csv"
 bolly = pd.read_csv(url)
-df = pd.DataFrame(bolly)
+# df = pd.DataFrame(bolly)
+bolly.rename(columns={'Lead Star': 'LeadStar', 'Release Period' : 'ReleasePeriod'}, inplace=True)
 #print(bolly[:10].to_string())
-#bolly.info()
-print(bolly.Genre.unique())
+bolly.info()
+# print(bolly.Genre.unique())
+
+
+actors = bolly.LeadStar.unique()
+actors.sort()
+
+directors = bolly.Director.unique()
+directors.sort()
+
 app = Flask(__name__)
+
 
 @app.route("/", methods=["GET", "POST"])
 def hello_world():
     if request.method == "GET":
         return render_template("websiteTemplate.html",
-                               genreOptions = bolly.Genre.unique(),)
+                               periodOptions=bolly.ReleasePeriod.unique(),
+                               genreOptions=bolly.Genre.unique(),
+                               actorOptions=actors,
+                               directorOptions=directors)
     else:
         return "working on it"
