75731cef2718a96150be1f79ddf9b7f9f46eddf1
Jasmine Xu <109494334+jasminex21@users.noreply.github.com>
2022-10-15 12:55:47 -0500

    new file

--- test.py
+++ test.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import numpy as np
 
-netflix = pd.read_csv(r"C:\Users\jasmi\OneDrive\Documents\UT\Personal Projects\netflix_titles.csv")
-print(netflix)
+url = "https://raw.githubusercontent.com/anjieliuanjie/HackTX/main/netflix_titles.csv"
+netflix = pd.read_csv(url)
+
+print(netflix[:10].to_string())
